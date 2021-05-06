66d540195c8959d1c5011de2765e1b4f1ae36e74
Fede Álvarez <fedealvarezcampos@gmail.com>
2021-05-06 15:36:03 +0200

    Link preview

--- controllers/posts-controller.js
+++ controllers/posts-controller.js
@@ -1,52 +1,49 @@
-const Joi = require("joi");
+const Joi = require('joi');
 
-const { postsRepository } = require("../repos");
+const { postsRepository } = require('../repos');
 
 async function createPost(req, res, next) {
-  try {
-    //Saber la id del usuario que está realizando la petición
-    const { id } = req.auth;
+    try {
+        //Saber la id del usuario que está realizando la petición
+        const { id } = req.auth;
 
-    //Saber que datos se están enviando en la petición
-    const { title, description } = req.body;
+        //Saber que datos se están enviando en la petición
+        const { title, description } = req.body;
 
-    //Validar que los datos que se envían son correctos
-    const schema = Joi.object({
-      title: Joi.string()
-        .max(255)
-        .error(() => new Error("Título muy largo. Máximo 255 caracteres.")),
-      description: Joi.string()
-        .max(255)
-        .error(
-          () =>
-            new Error("Te pasaste con la descripción. Máximo 255 caracteres.")
-        ),
-    });
+        //Validar que los datos que se envían son correctos
+        const schema = Joi.object({
+            title: Joi.string()
+                .max(255)
+                .error(() => new Error('Too long of a title. 255 chars max.')),
+            description: Joi.string()
+                .max(255)
+                .error(() => new Error('Too long of a description, max 255 characters.')),
+        });
 
-    await schema.validateAsync({ title, description });
+        await schema.validateAsync({ title, description });
 
-    //Meter los datos en la base de datos (usando una función externa)
-    const result = await postsRepository.insertPost({
-      userId: id,
-      title,
-      description,
-    });
+        //Meter los datos en la base de datos (usando una función externa)
+        const result = await postsRepository.insertPost({
+            userId: id,
+            title,
+            description,
+        });
 
-    //Devolver información al usuario de que todo fue bien
-    return res.send({
-      status: "ok",
-      data: {
-        id_user: id,
-        id_post: result.insertId,
-        title,
-        description,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
+        //Devolver información al usuario de que todo fue bien
+        return res.send({
+            status: 'ok',
+            data: {
+                id_user: id,
+                id_post: result.insertId,
+                title,
+                description,
+            },
+        });
+    } catch (error) {
+        next(error);
+    }
 }
 
 module.exports = {
-  createPost,
+    createPost,
 };
