c6670ac0fecc670e0e33822d0e4efc0aa6e66346
Fede Álvarez <fedealvarezcampos@gmail.com>
2021-05-06 19:08:58 +0200

    get recent posts @ posts

--- controllers/posts-controller.js
+++ controllers/posts-controller.js
@@ -1,67 +1,108 @@
-const Joi = require("joi");
+const Joi = require('joi');
 
-const { postsRepository } = require("../repos");
+const { postsRepository } = require('../repos');
+
+async function getPost(req, res, next) {
+    try {
+        const { id } = req.params;
+
+        const post = await postsRepository.getPostById(id);
+
+        if (!post) {
+            const err = new Error(`Post does not exist.`);
+            err.code = 409;
+
+            throw err;
+        }
+
+        res.send({
+            id: post.id,
+            title: post.title,
+            link: post.link,
+            description: post.description,
+            date: post.created_date,
+        });
+    } catch (err) {
+        next(err);
+    }
+}
+
+async function getRecentPosts(req, res, next) {
+    try {
+        const posts = await postsRepository.sortPostsByDate();
+
+        if (!posts) {
+            const err = new Error(`No posts.`);
+            err.code = 409;
+
+            throw err;
+        }
+
+        res.send(posts);
+    } catch (err) {
+        next(err);
+    }
+}
 
 async function createPost(req, res, next) {
-  try {
-    //Saber la id del usuario que está realizando la petición
-    const { id } = req.auth;
+    try {
+        const { id } = req.auth;
 
-    //Saber que datos se están enviando en la petición
-    const { link, title, description } = req.body;
+        //Saber que datos se están enviando en la petición
+        const { link, title, description } = req.body;
 
-    //Validar que los datos que se envían son correctos
-    const schema = Joi.object({
-      link: Joi.string().uri().required(),
-      title: Joi.string()
-        .max(255)
-        .error(() => new Error("Too long of a title. 255 chars max.")),
-      description: Joi.string()
-        .max(255)
-        .error(
-          () => new Error("Too long of a description, max 255 characters.")
-        ),
-    });
+        //Validar que los datos que se envían son correctos
+        const schema = Joi.object({
+            link: Joi.string().uri().required(),
+            title: Joi.string()
+                .max(255)
+                .error(() => new Error('Too long of a title. 255 chars max.')),
+            description: Joi.string()
+                .max(255)
+                .error(() => new Error('Too long of a description, max 255 characters.')),
+        });
 
-    await schema.validateAsync({ link, title, description });
+        await schema.validateAsync({ link, title, description });
 
-    //Meter los datos en la base de datos (usando una función externa)
-    const result = await postsRepository.insertPost({
-      link,
-      userId: id,
-      title,
-      description,
-    });
+        //Meter los datos en la base de datos (usando una función externa)
+        const result = await postsRepository.insertPost({
+            link,
+            userId: id,
+            title,
+            description,
+        });
 
-    //Devolver información al usuario de que todo fue bien
-    return res.send({
-      status: "ok",
-      data: {
-        link,
-        id_user: id,
-        id_post: result.insertId,
-        title,
-        description,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
+        //Devolver información al usuario de que todo fue bien
+        return res.send({
+            status: 'ok',
+            data: {
+                link,
+                id_user: id,
+                id_post: result.insertId,
+                title,
+                description,
+            },
+        });
+    } catch (error) {
+        next(error);
+    }
 }
 
 async function deletePost(req, res, next) {
-  try {
-    const { id } = req.params;
+    try {
+        const { id } = req.params;
 
-    const result = await postsRepository.deletePost(id);
+        const result = await postsRepository.deletePost(id);
 
-    return res.send(id);
-  } catch (error) {
-    next(error);
-  }
+        return res.send(result);
+    } catch (error) {
+        next(error);
+    }
 }
 
 module.exports = {
-  createPost,
-  deletePost,
+    createPost,
+    deletePost,
+    getPost,
+    getRecentPosts,
 };
--- repos/posts-repo.js
+++ repos/posts-repo.js
@@ -1,29 +1,45 @@
-const SQL = require("sql-template-strings");
-const { database } = require("../infrastructure");
+const SQL = require('sql-template-strings');
+const { database } = require('../infrastructure');
+
+const getPostById = async id => {
+    const query = SQL`SELECT * FROM posts WHERE id = ${id}`;
+    const [posts] = await database.pool.query(query);
+
+    return posts[0];
+};
+
+const sortPostsByDate = async () => {
+    const query = SQL`SELECT * FROM posts ORDER BY created_date DESC`;
+    const [posts] = await database.pool.query(query);
+
+    return posts;
+};
 
 const insertPost = async ({ link, userId, title, description }) => {
-  //creo la query
-  const query = SQL`
+    //creo la query
+    const query = SQL`
     INSERT INTO posts(userId, title, description, link, created_date)
     VALUES(${userId}, ${title}, ${description}, ${link}, ${new Date()});
   `;
 
-  //Ejecuto la query
-  const [result] = await database.pool.query(query);
+    //Ejecuto la query
+    const [result] = await database.pool.query(query);
 
-  //Devuelvo información del resultado
-  return result;
+    //Devuelvo información del resultado
+    return result;
 };
 
-const deletePost = async (id) => {
-  const query = SQL`DELETE FROM posts WHERE id = ${id}`;
+const deletePost = async id => {
+    const query = SQL`DELETE FROM posts WHERE id = ${id}`;
 
-  const [result] = await database.pool.query(query);
+    const [result] = await database.pool.query(query);
 
-  return result;
+    return result;
 };
 
 module.exports = {
-  insertPost,
-  deletePost,
+    insertPost,
+    deletePost,
+    getPostById,
+    sortPostsByDate,
 };
