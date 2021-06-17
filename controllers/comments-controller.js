473e1db6a0837c16262f1701ec18cb3bdf170e08
Fede Álvarez <fedealvarezcampos@gmail.com>
2021-06-17 22:06:57 +0200

    Error handling in comments controller

--- controllers/comments-controller.js
+++ controllers/comments-controller.js
@@ -32,7 +32,11 @@ async function postComment(req, res, next) {
         const { id: postId } = req.params;
         const { id } = req.auth;
 
-        const schema = Joi.string().min(5).max(500);
+        const schema = Joi.string()
+            .min(5)
+            .max(500)
+            .error(() => new Error(`Say something that's between 5 and 250 characters.`));
+
         await schema.validateAsync(text);
 
         const user = await usersRepository.getUserById(id);
