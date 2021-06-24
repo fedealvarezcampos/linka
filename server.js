bd35fba34d416b08a8061d6b38196ec304adc888
Fede Álvarez <fedealvarezcampos@gmail.com>
2021-06-25 00:27:58 +0200

    Fighting CORS, round 2

--- server.js
+++ server.js
@@ -13,10 +13,17 @@ const staticPath = path.resolve(__dirname, 'static');
 
 const app = express();
 
-const whitelist = ['http://localhost', 'https://linkah.vercel.app/'];
+const whitelist = [
+    `http://localhost:3000`,
+    `http://localhost:3000/`,
+    `http://localhost:8080`,
+    `http://localhost:8080/`,
+    `https://linkah.vercel.app/`,
+    `https://linkah.vercel.app`,
+];
 const corsOptions = {
     origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1) {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
             callback(null, true);
         } else {
             callback(new Error('Not allowed by CORS'));
