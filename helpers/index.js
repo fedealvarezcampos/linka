90838c9b807912e58736a1a7ce4caf311afb6159
Fede Álvarez <fedealvarezcampos@gmail.com>
2021-06-25 17:04:42 +0200

    AWS magics

--- helpers/index.js
+++ helpers/index.js
@@ -4,50 +4,69 @@ const UUID = require('uuid');
 const path = require('path');
 const AWS = require('aws-sdk');
 
-const { AWSAccessKeyId, AWSSecretKey, AWSBUCKETNAME } = process.env;
-
-AWS.config.update({
-    accessKeyId: AWSAccessKeyId, // Access key ID
-    secretAccesskey: AWSSecretKey, // Secret access key
-    region: 'eu-west-3', //Region
-});
+const { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION, S3_BUCKET } = process.env;
 
 const uploadDir = path.join(__dirname, process.env.UPLOADPATH);
 
-const uploadImages = async ({ file, dir, userAvatar }) => {
-    const targetDir = path.join(uploadDir, dir);
+// const uploadImages = async ({ file, dir, userAvatar }) => {
+//     const targetDir = path.join(uploadDir, dir);
 
-    userAvatar && fs.unlink(targetDir + '/' + userAvatar);
+//     userAvatar && fs.unlink(targetDir + '/' + userAvatar);
 
-    await fs.mkdir(targetDir, { recursive: true });
+//     await fs.mkdir(targetDir, { recursive: true });
 
-    const image = sharp(file.data);
-    const imageInfo = await image.metadata();
+//     const image = sharp(file.data);
+//     const imageInfo = await image.metadata();
 
-    if (imageInfo.width > 500) {
-        image.resize(500);
+//     if (imageInfo.width > 500) {
+//         image.resize(500);
+//     }
+
+//     const imageName = `AV${UUID.v4()}.jpg`;
+
+//     await image.toFile(path.join(targetDir, imageName));
+
+//     return imageName;
+// };
+
+async function uploadToS3(req, res, next) {
+    try {
+        AWS.config.update({
+            accessKeyId: AWS_ACCESS_KEY_ID, // Access key ID
+            secretAccesskey: AWS_SECRET_ACCESS_KEY, // Secret access key
+            region: AWS_REGION, //Region
+        });
+
+        const s3 = new AWS.S3();
+
+        console.log(s3);
+
+        const fileContent = Buffer.from(req.files.avatar.data);
+        console.log(fileContent);
+
+        const params = {
+            Bucket: S3_BUCKET,
+            Key: `AV${UUID.v4()}.jpg`, // File name you want to save as in S3
+            Body: fileContent,
+            Expires: 60,
+            ContentType: 'image/jpeg',
+            ACL: 'public-read',
+        };
+
+        // Uploading files to the bucket
+        s3.upload(params, function (err) {
+            if (err) {
+                throw err;
+            }
+        });
+
+        next();
+    } catch (err) {
+        next(err);
     }
-
-    const imageName = `AV${UUID.v4()}.jpg`;
-
-    await image.toFile(path.join(targetDir, imageName));
-    console.log(file);
-
-    const s3 = new AWS.S3();
-
-    const awsParams = {
-        Bucket: AWSBUCKETNAME,
-        Key: imageName,
-        Body: file,
-        Expires: 60,
-        ACL: 'public-read',
-    };
-
-    s3.upload(awsParams);
-
-    return imageName;
-};
+}
 
 module.exports = {
     uploadImages,
+    uploadToS3,
 };
